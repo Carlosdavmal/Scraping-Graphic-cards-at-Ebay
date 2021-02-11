1b1c1c5e59890b0d1b94d4d4045688dcc7965703
Carlos <77513051+Carlosdavmal@users.noreply.github.com>
2021-02-11 18:23:13 -0500

    Add files via upload

---
+++ Ebay.py
@@ -0,0 +1,72 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+
+def get_page(url):
+    response = requests.get(url)
+
+    if not response.ok:
+        print('Ebay is: ', response.status_code)
+    else:
+        soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
+def get_data(soup):
+    try:
+        name = soup.find('h1', id ='itemTitle').get_text().replace('Detalles acerca de ','').replace('\xa0', '',)
+    except:
+        name = ''
+    
+    try:
+        p = soup.find('span', id='prcIsum_bidPrice').text.strip()
+        currency, price = p.split(' ')
+    except:
+        currency = ''
+        price = ''
+
+    try:
+        time_left = soup.find('span', id='vi-cdown_timeLeft').text.strip()
+        
+    except:
+        time_left = ''
+        
+
+    data = {'name': name,
+            'price': price,
+            'currency': currency,
+            'time_left': time_left
+            }
+
+    return data
+    
+def get_list_data(soup):
+    try:
+        links = soup.find_all('a', class_='s-item__link')
+
+    except:
+        links = []
+
+    
+    urls = [item.get('href') for item in links]
+    return urls
+
+def csv_file(data, url):
+    with open('RTX_scraped.csv', 'a', encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+
+        row = [data['name'], data['price'], data['currency'], data['time_left'], url]
+       
+        writer.writerow(row)
+
+def main():
+    url ='https://www.ebay.com/sch/i.html?&_nkw=Rtx&_pgn=1'
+    
+    products = get_list_data(get_page(url))
+
+    for link in products:
+        data = get_data(get_page(link))
+        csv_file(data, link)
+       
+if __name__ == '__main__':
+    main()
